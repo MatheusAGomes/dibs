d313d1d3b7bb65ad88647488ce879a1bacc76738
Matheus Gomes <matheusgoassuncao@gmail.com>
2023-09-04 19:59:42 -0300

    banner de cartoes ainda nao clicavel

---
+++ lib/widget/meuCartaoComponente.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../shared/service/colorService.dart';
+
+class MeuCartaoComponente extends StatelessWidget {
+  String? numero;
+  String? tipo;
+
+  MeuCartaoComponente({
+    required this.numero,
+    required this.tipo,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.grey.shade500,
+            borderRadius: BorderRadius.circular(11)),
+        height: 50,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Icon(Icons.credit_card),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(numero!),
+                  ],
+                ),
+                Text(tipo!)
+              ],
+            ),
+            Icon(Icons.delete_outline_outlined)
+          ]),
+        ),
+      ),
+    );
+  }
+}
--- lib/widget/modalMeusCartoes.dart
+++ lib/widget/modalMeusCartoes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../shared/service/colorService.dart';
+import 'meuCartaoComponente.dart';
 
 class ModalMeusCartoes extends StatelessWidget {
   ModalMeusCartoes();
@@ -10,24 +11,41 @@ class ModalMeusCartoes extends StatelessWidget {
     return SafeArea(
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Meus cartões',
-                  style: TextStyle(fontWeight: FontWeight.w900, fontSize: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Meus cartões',
+                    style: TextStyle(fontWeight: FontWeight.w900, fontSize: 20),
+                  ),
+                  InkWell(
+                    child: Icon(Icons.close),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.1,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: (context, index) => Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: MeuCartaoComponente(
+                        numero: "1234123412341234", tipo: "Crédito"),
+                  ),
                 ),
-                InkWell(
-                  child: Icon(Icons.close),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              ],
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
