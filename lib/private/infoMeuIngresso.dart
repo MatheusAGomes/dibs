114e643b9c61b5096ec19624d37268df701b35b8
matheus <matheusgoassuncao@gmail.com>
2023-09-07 13:41:59 -0300

    conteudo do ingresso

---
+++ lib/private/infoMeuIngresso.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+import '../shared/service/colorService.dart';
+import '../widget/bannerIngressoMarketPlace.dart';
+
+class InfoMeuIngressoScreen extends StatelessWidget {
+  String? nomeDoTitular;
+  String? cpf;
+  String? codigoDoIngresso;
+  ImageProvider? fotoDoEvento;
+  InfoMeuIngressoScreen({
+    required this.codigoDoIngresso,
+    required this.cpf,
+    required this.nomeDoTitular,
+    required this.fotoDoEvento,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Stack(children: <Widget>[
+            Padding(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.25,
+                    left: 19,
+                    right: 19),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 25),
+                      child: Column(
+                        children: [
+                          QrImage(
+                            data: codigoDoIngresso!,
+                            version: QrVersions.auto,
+                            size: 250.0,
+                          ),
+                          Center(child: Text(codigoDoIngresso!)),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Titular',
+                          style: TextStyle(fontWeight: FontWeight.w900),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(nomeDoTitular!),
+                      ],
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.02,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'CPF',
+                          style: TextStyle(fontWeight: FontWeight.w900),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(cpf!),
+                      ],
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.02,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.3,
+                          child: TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                'Transferir',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                      Color(0xff198A68)))),
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.3,
+                          child: TextButton(
+                              style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                      Color(0xff198A68))),
+                              onPressed: () {},
+                              child: Text(
+                                'Editar',
+                                style: TextStyle(color: Colors.white),
+                              )),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.02,
+                    ),
+                  ],
+                )),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                image: DecorationImage(fit: BoxFit.cover, image: fotoDoEvento!),
+              ),
+              height: MediaQuery.of(context).size.height * 0.25,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: IconButton(
+                  icon: Icon(Icons.arrow_back_ios),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
--- lib/widget/bannerMeuIngresso.dart
+++ lib/widget/bannerMeuIngresso.dart
@@ -1,3 +1,4 @@
+import 'package:dibs/private/infoMeuIngresso.dart';
 import 'package:flutter/material.dart';
 
 import '../shared/service/colorService.dart';
@@ -24,76 +25,90 @@ class BannerMeuIgresso extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 200,
-      child: Stack(alignment: Alignment.bottomLeft, children: [
-        Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(fit: BoxFit.cover, image: image),
-              borderRadius: BorderRadiusDirectional.circular(10)),
-          width: 300,
-        ),
-        Container(
-          height: 70,
-          width: 300,
-          decoration: BoxDecoration(
-            color: corBanner,
-            borderRadius: BorderRadiusDirectional.only(
-                bottomEnd: Radius.circular(10),
-                bottomStart: Radius.circular(10)),
+    return InkWell(
+      onTap: () => showModalBottomSheet<void>(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext context) {
+          return InfoMeuIngressoScreen(
+            codigoDoIngresso: 'de7a89f51g477c82e23f68g',
+            cpf: '222.222.222-22',
+            fotoDoEvento: image,
+            nomeDoTitular: 'Matheus Assunção Gomes',
+          );
+        },
+      ),
+      child: SizedBox(
+        height: 200,
+        child: Stack(alignment: Alignment.bottomLeft, children: [
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(fit: BoxFit.cover, image: image),
+                borderRadius: BorderRadiusDirectional.circular(10)),
+            width: 300,
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 10),
-          child: SizedBox(
-              height: 70,
-              width: 300,
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Row(
-                        children: [
-                          Icon(Icons.calendar_month_outlined),
-                          Text(data)
-                        ],
-                      ),
-                      SizedBox(
-                        width: 30,
-                      ),
-                      Row(
-                        children: [Icon(Icons.schedule), Text(hora)],
-                      )
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        titulo,
-                        style: TextStyle(fontWeight: FontWeight.w900),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        lote,
-                        style: TextStyle(
-                            color: corDoLote, fontWeight: FontWeight.w900),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        tipo,
-                        style: TextStyle(color: Colors.black54),
-                      )
-                    ],
-                  )
-                ],
-              )),
-        ),
-      ]),
+          Container(
+            height: 70,
+            width: 300,
+            decoration: BoxDecoration(
+              color: corBanner,
+              borderRadius: BorderRadiusDirectional.only(
+                  bottomEnd: Radius.circular(10),
+                  bottomStart: Radius.circular(10)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: SizedBox(
+                height: 70,
+                width: 300,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.calendar_month_outlined),
+                            Text(data)
+                          ],
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Row(
+                          children: [Icon(Icons.schedule), Text(hora)],
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          titulo,
+                          style: TextStyle(fontWeight: FontWeight.w900),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          lote,
+                          style: TextStyle(
+                              color: corDoLote, fontWeight: FontWeight.w900),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          tipo,
+                          style: TextStyle(color: Colors.black54),
+                        )
+                      ],
+                    )
+                  ],
+                )),
+          ),
+        ]),
+      ),
     );
   }
 }
