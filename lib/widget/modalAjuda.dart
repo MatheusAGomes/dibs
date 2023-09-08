f5590757a08d2fc15cc918ab5262f6a78486646c
matheus <matheusgoassuncao@gmail.com>
2023-09-08 09:22:55 -0300

    botao na ajuda

--- lib/widget/modalAjuda.dart
+++ lib/widget/modalAjuda.dart
@@ -13,114 +13,134 @@ class ModalAjuda extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Ajuda',
-                  style: TextStyle(fontWeight: FontWeight.w900, fontSize: 20),
-                ),
-                InkWell(
-                  child: Icon(Icons.close),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              children: [
-                Text(
-                  'Como posso te ajudar ?',
-                  style: TextStyle(fontWeight: FontWeight.w900),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 30,
-            ),
             Column(
               children: [
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('Assunto',
-                        style: TextStyle(fontWeight: FontWeight.w900)),
+                    Text(
+                      'Ajuda',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w900, fontSize: 20),
+                    ),
+                    InkWell(
+                      child: Icon(Icons.close),
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                    ),
                   ],
                 ),
-                Container(
-                  height: 34,
-                  width: 343,
-                  child: TextFormField(
-                    cursorColor: Colors.grey,
-                    controller: assuntoController,
-                    decoration: InputDecoration(
-                      contentPadding:
-                          EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(7),
-                          borderSide: BorderSide(
-                              width: 1, color: Colors.grey.shade300)),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(7),
-                          borderSide: BorderSide(width: 1, color: Colors.grey)),
-                    ),
-                  ),
+                SizedBox(
+                  height: 30,
                 ),
-              ],
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Column(
-              children: [
                 Row(
                   children: [
-                    Text('Pergunta/Reclamação',
+                    Text(
+                      'Como posso te ajudar ?',
+                      style: TextStyle(fontWeight: FontWeight.w900),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text('Assunto',
+                            style: TextStyle(fontWeight: FontWeight.w900)),
+                      ],
+                    ),
+                    Container(
+                      height: 34,
+                      width: 343,
+                      child: TextFormField(
+                        cursorColor: Colors.grey,
+                        controller: assuntoController,
+                        decoration: InputDecoration(
+                          contentPadding:
+                              EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(7),
+                              borderSide: BorderSide(
+                                  width: 1, color: Colors.grey.shade300)),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(7),
+                              borderSide:
+                                  BorderSide(width: 1, color: Colors.grey)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text('Pergunta/Reclamação',
+                            style: TextStyle(fontWeight: FontWeight.w900)),
+                      ],
+                    ),
+                    Container(
+                      height: 34,
+                      width: 343,
+                      child: TextFormField(
+                        cursorColor: Colors.grey,
+                        controller: assuntoController,
+                        decoration: InputDecoration(
+                          contentPadding:
+                              EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(7),
+                              borderSide: BorderSide(
+                                  width: 1, color: Colors.grey.shade300)),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(7),
+                              borderSide:
+                                  BorderSide(width: 1, color: Colors.grey)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    Text('Email para contato',
                         style: TextStyle(fontWeight: FontWeight.w900)),
                   ],
                 ),
-                Container(
-                  height: 34,
-                  width: 343,
-                  child: TextFormField(
-                    cursorColor: Colors.grey,
-                    controller: assuntoController,
-                    decoration: InputDecoration(
-                      contentPadding:
-                          EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(7),
-                          borderSide: BorderSide(
-                              width: 1, color: Colors.grey.shade300)),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(7),
-                          borderSide: BorderSide(width: 1, color: Colors.grey)),
-                    ),
-                  ),
+                Row(
+                  children: [
+                    Text('suporte@trocatroca.com',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            color: Colors.grey,
+                            decoration: TextDecoration.underline)),
+                  ],
                 ),
               ],
             ),
             SizedBox(
-              height: 30,
-            ),
-            Row(
-              children: [
-                Text('Email para contato',
-                    style: TextStyle(fontWeight: FontWeight.w900)),
-              ],
-            ),
-            Row(
-              children: [
-                Text('suporte@trocatroca.com',
-                    style: TextStyle(
-                        fontWeight: FontWeight.w900,
-                        color: Colors.grey,
-                        decoration: TextDecoration.underline)),
-              ],
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.orange)),
+                onPressed: () {},
+                child: const Text(
+                  'Enviar',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
             ),
           ],
         ),
