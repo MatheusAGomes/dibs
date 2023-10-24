4595483c9a2a91b8b61a0fd16d19a144bb5f3b27
luisciarbello <lg10_ciarbello@hotmail.com>
2023-10-23 22:16:23 -0300

    + encapsulamento do text field;

---
+++ lib/widget/expandableTextField.dart
@@ -0,0 +1,100 @@
+import 'package:dibs/widget/textfieldpadrao.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ExpandableTextField extends StatefulWidget{
+  final GlobalKey<FormFieldState>? textFormFildKey;
+  final TextEditingController? controller;
+  final String? hintText;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final TextInputType keyboardtype;
+  final bool hideTextfild;
+  final VoidCallback click;
+  final double fontSize;
+  final TextInputAction? textInputAction;
+  final Function(String?)? onFieldSubmitted;
+  final String? Function(String?)? validator;
+  final String? Function(String?)? onchange;
+  final FocusNode? focusNode;
+  final String? errorText;
+  final dynamic padding;
+  final int? maxlength;
+  final bool? enable;
+  List<TextInputFormatter>? inputFormatter;
+  final double height;
+
+
+  ExpandableTextField({
+    this.textFormFildKey,
+    this.hintText,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.keyboardtype = TextInputType.text,
+    this.hideTextfild = false,
+    required this.click,
+    this.fontSize = 10,
+    this.validator,
+    this.controller,
+    this.onchange,
+    this.textInputAction,
+    this.onFieldSubmitted,
+    this.focusNode,
+    this.errorText,
+    this.padding,
+    this.maxlength,
+    this.enable,
+    this.inputFormatter,
+    required this.height,
+  });
+
+
+  @override
+  State<ExpandableTextField> createState() => _ExpandableTextFieldState();
+}
+
+class _ExpandableTextFieldState extends State<ExpandableTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: MediaQuery.of(context).size.height * widget.height,
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(7),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    color: Color(0x40000000),
+                    offset: Offset(2,2),
+                    blurRadius: 0.5
+                )
+              ]
+          ),
+          child: TextFieldPadrao(
+            textFormFildKey: widget.textFormFildKey,
+            hintText: widget.hintText,
+            prefixIcon: widget.prefixIcon,
+            suffixIcon: widget.suffixIcon,
+            keyboardtype: widget.keyboardtype,
+            hideTextfild: widget.hideTextfild,
+            click: () {
+              widget.click();
+            },
+            fontSize: widget.fontSize,
+            validator: widget.validator,
+            controller: widget.controller,
+            onchange: widget.onchange,
+            textInputAction: widget.textInputAction,
+            onFieldSubmitted: widget.onFieldSubmitted,
+            focusNode: widget.focusNode,
+            errorText: widget.errorText,
+            padding: widget.padding,
+            maxlength: widget.maxlength,
+            enable: widget.enable,
+            inputFormatter: widget.inputFormatter,
+          ),
+        )
+    );
+  }
+}
